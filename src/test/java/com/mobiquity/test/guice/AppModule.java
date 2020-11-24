13996dcf7ed494f6ba3903714707f11bf2cdb724
Sujit Mohanan <xsujit@gmail.com>
2020-11-24 19:54:06 +0000

    Added Guice Injector and logger

---
+++ src/test/java/com/mobiquity/test/guice/AppModule.java
@@ -0,0 +1,11 @@
+package com.mobiquity.test.guice;
+
+import com.google.inject.AbstractModule;
+
+public class AppModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        super.configure();
+    }
+}
