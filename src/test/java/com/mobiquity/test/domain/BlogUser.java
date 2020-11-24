7cfea7e50fa59ecff0799943e39e111a86269b28
Sujit Mohanan <xsujit@gmail.com>
2020-11-24 21:26:51 +0000

    Get matching user

--- src/test/java/com/mobiquity/test/domain/BlogUser.java
+++ src/test/java/com/mobiquity/test/domain/BlogUser.java
@@ -1,6 +1,9 @@
 package com.mobiquity.test.domain;
 
 import io.cucumber.guice.ScenarioScoped;
+import io.restassured.RestAssured;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @ScenarioScoped
 public class BlogUser {
@@ -13,5 +16,26 @@ public class BlogUser {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public JSONObject getMyDetails() {
+        JSONArray users = getUsers();
+        for (int i = 0; i <users.length(); i++) {
+            JSONObject user = users.getJSONObject(i);
+            if (user.getString("username").equalsIgnoreCase(username))
+                return user;
+        }
+        return null;
+    }
+
+    private JSONArray getUsers() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        String body = RestAssured.get("/users")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .asString();
+        return new JSONArray(body);
     }
 }
